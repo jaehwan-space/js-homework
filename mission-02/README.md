3e0fed3fc97e45189604a35dac2d1a4a565f4430
jaehwan-space <ma2582m@naver.com>
2024-06-30 19:30:22 +0900

    [6월 30일] 미션2 과제 완료

---
+++ mission-02/README.md
@@ -0,0 +1,5 @@
+# Poster UI
+
+---
+
+사이트 보러가기 : [[클릭]]('https://jaehwan-space.github.io/js-homework/mission-02/index.html')
--- mission-02/js/main.js
+++ mission-02/js/main.js
@@ -1,24 +1,58 @@
+const nav = document.querySelector(".nav");
+const img = document.querySelector(".visual img");
+const name = document.querySelector(".nickName");
 
-/* 
+const audios = data.map((element) => {
+  return new AudioPlayer(`./assets/audio/${element.name.toLowerCase()}.m4a`);
+});
 
-1. 클릭 이벤트 활성화
-2. nav 클릭시 배경 색상 변경
-3. 이미지 변경
-4. 텍스트 변경
-5. 함수 분리
+function setBgColor(colorA, colorB) {
+  document.body.style.background = `linear-gradient(to bottom, ${colorA},${colorB})`;
+}
 
-*/
+function setImage(target, data) {
+  target.src = `./assets/${data.name}.jpeg`;
+  target.alt = data.alt;
+}
 
+function setNameText(target, data) {
+  target.textContent = data.name;
+}
 
+function removeActives() {
+  const liLists = document.querySelectorAll(".nav li");
 
+  liLists.forEach((element) => {
+    element.classList.remove("is-active");
+  });
+}
 
+function stopAudios() {
+  audios.forEach((element) => {
+    element.stop();
+  });
+}
 
+function handleClick(e) {
+  const target = e.target.closest("li");
 
+  if (!target) return;
 
+  const index = target.dataset.index;
+  const selectedData = data[index - 1];
 
+  // 배경, 이미지, 텍스트 변경
+  setBgColor(selectedData.color[0], selectedData.color[1]);
+  setImage(img, selectedData);
+  setNameText(name, selectedData);
 
+  // 'is-acitve' 클래스 제거 및 추가
+  removeActives();
+  target.classList.add("is-active");
 
+  // 오디오 제어
+  stopAudios();
+  audios[index - 1].play();
+}
 
-
-
-
+nav.addEventListener("click", handleClick);
